076f41cc14d61f5844797a1d90f8ed17168c081c
CarolinePersson <carolinepersson97@outlook.com>
2026-02-09 21:04:42 +0100

    projekt ID

--- src/app/dashboard/projects/[id]/page.tsx
+++ src/app/dashboard/projects/[id]/page.tsx
@@ -1,23 +1,48 @@
-import {createClient} from "@/lib/supabase/server";
+import { createClient } from "@/lib/supabase/server";
+import { Field, FieldGroup, FieldTitle } from "@/components/ui/field";
 
-type props = {
+type Props = {
     params: {
-        id: string
-    }
-}
+        id: string;
+    };
+};
 
-export default async function Page({params}: props) {
-
-    const {id} = await params;
-
+export default async function Page({ params }: Props) {
     const supabase = await createClient();
-    const {data: project} = await supabase.from('projects').select().eq('id', id).single();
 
+    const { data: project, error } = await supabase
+        .from("projects")
+        .select()
+        .eq("id", params.id)
+        .single();
+
+    if (error || !project) {
+        return <div>Projektet hittades inte</div>;
+    }
 
     return (
         <div>
-            <pre>{JSON.stringify(project, null, 3)}</pre>
-        </div>
-    )
+            <h1>Project</h1>
 
+            <FieldGroup>
+                <Field>
+                    <FieldTitle>Project name</FieldTitle>
+                    <div>{project.name}</div>
+                </Field>
+
+                <Field>
+                    <FieldTitle>Project ID</FieldTitle>
+                    <div>{project.id}</div>
+                </Field>
+
+                <Field>
+                    <FieldTitle>Created</FieldTitle>
+                    <div>
+                        {new Date(project.created_at).toLocaleDateString("sv-SE")}
+                    </div>
+                </Field>
+            </FieldGroup>
+        </div>
+    );
 }
+
